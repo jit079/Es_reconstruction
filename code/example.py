7431743c6801bd82118ce3784261675556ae4d80
jit079 <118217674+jit079@users.noreply.github.com>
2023-11-29 11:55:03 -0800

    Update example.py
    Adding comments to code

--- code/example.py
+++ code/example.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 Ed=[0.2,0.25,0.23,0.2]
 
-sz=30.
+sz=30. # solar zenith angle corresponding to the input Ed observation time
 
 merra2_file='../merra2/MERRA2_401.tavg1_2d_slv_Nx.20200929.SUB.nc'
 ozone,water,pressure=get_MERRA2(merra2_file,12,0,0)
@@ -12,7 +12,7 @@ ozone,water,pressure=get_MERRA2(merra2_file,12,0,0)
 
 print(ozone,water,pressure)
 
-output_wl=np.arange(315,900,5)
+output_wl=np.arange(315,900,5) # the output wavelengths that you want to reconstruct Ed at
 
 Ed_reconstructed=reconstruct_Es(sz,ozone,pressure,water,Ed,output_wl)
 
